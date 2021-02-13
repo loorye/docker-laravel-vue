f49850d9d4e411b5cfbe72ccc92d82da483798d6
loorye <no.limit2011.okawari60@gmail.com>
2021-02-13 15:02:07 +0900

    jsファイルのセミコロンを追加

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -11,13 +11,13 @@ import router from './router';
 import store from './store';
 import App from './App.vue';
 
-import ElementUI from 'element-ui'
-import locale from 'element-ui/lib/locale/lang/ja'
-import 'element-ui/lib/theme-chalk/index.css'
-import 'element-ui/lib/theme-chalk/display.css'
+import ElementUI from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/ja';
+import 'element-ui/lib/theme-chalk/index.css';
+import 'element-ui/lib/theme-chalk/display.css';
 
-Vue.config.productionTip = false
-Vue.use(ElementUI, locale)
+Vue.config.productionTip = false;
+Vue.use(ElementUI, locale);
 
 new Vue({
   router,
--- src/resources/js/router/index.js
+++ src/resources/js/router/index.js
@@ -21,4 +21,4 @@ export default new Router({
       component: ExampleComponent
     },
   ]
-})
+});
